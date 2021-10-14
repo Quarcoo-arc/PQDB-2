8b0e9ac0abc41b5ce84c4365c3d6e880183eec98
Sarah <mansahmensah18@gmail.com>
2021-10-14 13:09:45 -0700

    This is an update of the initial commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const body_parser = require('body-parser');
 const bcrypt = require('bcryptjs');
+const Schema = mongoose.Schema;
 
 const exp = express();
 const bodyParser = body_parser;
@@ -12,7 +13,7 @@ exp.use(bodyParser.urlencoded({
     extended: true,
 }));
 
-mongoose.connect('mongodb://localhost:27017/usersdb', {
+mongoose.connect('mongodb://localhost:27017/studentsdb', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
@@ -22,31 +23,61 @@ const db = mongoose.connection;
 db.on('error', () => console.log('Database connection error'));
 db.once('open', () => console.log('Database connected'));
 
-exp.post('/signup', (req, res) => {
-    const first_name = req.body.first_name;
-    const last_name = req.body.last_name;
-    const ref_no = req.body.ref_no;
-    const e_mail = req.body.e_mail;
-    const user_name = req.body.user_name;
-    const password = req.body.password;
 
-        const user_data = {
-        "_id": ref_no,
-        "first_name": first_name,
-        "last_name": last_name,
-        "e-mail": e_mail,
-        "user_name": user_name,
-        "password": password
+const userSchema = Schema ({
+    ref_no: {type: Number, required: true},
+    first_name: {type: String, required: true},
+    last_name: {type: String, required: true},
+    email: {type: String, required: true},
+    username : {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+},  {collection: 'Student' })
+
+const User = mongoose.model('User', userSchema);
+
+exp.post('/signup', async (req, res) => {
+    const {ref_no, first_name, last_name, email, username, password: plainPassword, password2} = req.body;
+
+    if (!ref_no || !first_name || !email || !username || !plainPassword || !password2) {
+        return res.json('Please fill in all the fields');
     }
 
-        db.collection('student').insertOne(user_data, (err, collection) => {
-        if(err){
-            throw err;
+    if (plainPassword !== password2) {
+        return res.json('Password does not match Confirmation Password');
         }
-        console.log('Record inserted successfully');
-    });
 
+    if (plainPassword.length < 8) {
+        return res.json('Password too short. Should be at least 8 characters');    
+        }
+
+    const password = await bcrypt.hash(plainPassword, 10);
+
+    try {
+        const response = await User.create({
+        ref_no, first_name, last_name, email, username, password
+        })
+        console.log('User created successfuly: ', response)
         return res.redirect('login.html');
+    }catch (error){
+        if (error.code === 11000) {
+            return res.json('Username already registered')
+        }
+        throw error
+    }
+})
+
+exp.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const user = await User.findOne({username: username});
+
+    if (!user){
+        return res.json('Invalid username or password')
+    }
+
+    if (await bcrypt.compare(password, user.password)) {
+        return res.redirect('dashboard.html');
+    }
+    res.json('Invalid username or password');    
 })
 
 exp.get('/', (req, res) => {
@@ -55,3 +86,5 @@ exp.get('/', (req, res) => {
 }).listen(3000);
 
 console.log('Listening from port 3000');
+
+
